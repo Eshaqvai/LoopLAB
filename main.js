4bfe92ed438718a1f1ffb72e00d268725b7dd137
Eshaqvai <129044084+Eshaqvai@users.noreply.github.com>
2023-03-28 01:15:51 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-dark navbar-expand-md fixed-top">
+    <div class="container">
+      <a href="#home-section" class="navbar-brand">LoopLAB</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#home-section">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#explore-head-section">Explore</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#create-head-section">Create</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#share-head-section">Share</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
